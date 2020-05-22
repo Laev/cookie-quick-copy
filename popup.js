7b77f7d81b75c869967e299aacae53108fbd185b
Laev <laihuiliang@aodiansoft.com>
2020-05-22 14:34:34 +0800

    update (基础版本已完成)

--- popup.js
+++ popup.js
@@ -1,74 +1,166 @@
-// TODO 设置cookie已完成，还需主动获取当前tab页的网址，需与另外的js通信
-document.addEventListener('DOMContentLoaded', function () {
-
-  document.querySelector('.popup-btn').addEventListener('click',async function(){
-    var fromUrl = document.querySelector('.from-url').value
-    var fromKey = document.querySelector('.from-key').value
-    var toUrl = document.querySelector('.to-url').value || 'http://test.laev.top/'
+// init
+document.addEventListener("DOMContentLoaded", function () {
+  setDefToUrl();
+  document
+    .querySelector(".popup-btn")
+    .addEventListener("click", async function () {
+      var fromUrl = document.querySelector(".from-url").value;
+      var fromKey = document.querySelector(".from-key").value;
+      var toUrl =
+        document.querySelector(".to-url").value || "http://test.laev.top/";
       if (!fromUrl && !fromKey) {
-        alert('url,key不能为空')
-        return false
+        alert("url,key不能为空");
+        return false;
       }
-
-    await permissionsContains(fromUrl)
-    const cookie = await getCookie(fromUrl, fromKey)
-    await permissionsContains(toUrl)
-    setCookie(toUrl, fromKey, cookie.value)
-  })
-
+      let cookie = {};
+      // 验证来源地址权限
+      const hasSourcePermission = await permissionsContains(fromUrl);
+      if (!hasSourcePermission) {
+        const isPassGet = await requestPermissions(fromUrl);
+        if (isPassGet) {
+          cookie = await getCookie(fromUrl, fromKey);
+        } else {
+          alert("操作不被允许，请授予权限");
+        }
+        return;
+      } else {
+        cookie = await getCookie(fromUrl, fromKey);
+      }
+      // 验证目标地址权限
+      const hasTargetPermission = await permissionsContains(toUrl);
+      const { value, path, expirationDate } = cookie;
+      if (!hasTargetPermission) {
+        const isPassSet = await requestPermissions(fromUrl);
+        if (isPassSet) {
+          setCookie(toUrl, fromKey, value, {
+            domain: getHostFromUrl(toUrl),
+            path,
+            expirationDate,
+          });
+        } else {
+          alert("操作不被允许，请授予权限");
+        }
+        return;
+      } else {
+        setCookie(toUrl, fromKey, value, {
+          domain: getHostFromUrl(toUrl),
+          path,
+          expirationDate,
+        });
+      }
+    });
 });
 
-function permissionsContains(url, opt = []) {
-  return new Promise(function(res, rej) {
-    chrome.permissions.contains({
-      permissions: ['cookies'],
-      origins: [url, ...opt]
-    }, function(result) {
-      if (result) {
-        res(result)
-      } else {
-        // 扩展程序没有这些权限。
-        alert('请前往插件设置打开权限')
-        rej(result)
-      }
-    })
-  })
+/**
+ * 获取激活tab host 及设置默认值
+ */
+function setDefToUrl() {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      document.querySelector(".to-url").value = tabs[0].url;
+    }
+  );
 }
 
+/**
+ * 获取地址host
+ */
+function getHostFromUrl(url) {
+  return new URL(url).hostname;
+}
+
+/**
+ * 查看权限
+ * @param  {string}  url  [需要请求相关权限的目标URL]
+ * @param  {array}   otherPermission  [额外权限]
+ */
+function permissionsContains(url, otherPermission = []) {
+  return new Promise((resolve) => {
+    chrome.permissions.contains(
+      {
+        permissions: ["cookies", ...otherPermission],
+        origins: [url],
+      },
+      function (res) {
+        resolve(res);
+      }
+    );
+  });
+}
+
+/**
+ * 请求权限
+ * @param  {string}  url  [需要请求相关权限的目标URL]
+ * @param  {array}   otherPermission  [额外权限]
+ */
+function requestPermissions(url, otherPermission = []) {
+  // 权限必须在用户操作下请求，例如按钮单击的事件处理函数。
+  return new Promise((resolve) => {
+    chrome.permissions.request(
+      {
+        permissions: ["cookies", ...otherPermission],
+        origins: [url],
+      },
+      function (granted) {
+        // 如果用户授予了这些权限，则回调函数的参数为 true。
+        resolve(granted);
+      }
+    );
+  });
+}
+
+/**
+ * 获取cookie
+ * @param  {string}  url  [需要获取的 Cookie 相关联的 URL]
+ * @param  {string}  name [cookie 名称]
+ * @param  {object}  opt  [额外参数]
+ */
 function getCookie(url, name, opt = {}) {
-  return new Promise(function (res, rej) {
-    chrome.cookies.get(({
-      url,
-      name,
-      ...opt
-    }),function(cookie) {
-      if (cookie) {
-        res(cookie)
-      } else {
-        rej(false)
+  return new Promise(function (resolve, reject) {
+    chrome.cookies.get(
+      {
+        url,
+        name,
+        ...opt,
+      },
+      function (cookie) {
+        if (cookie) {
+          resolve(cookie);
+        } else {
+          reject(false);
+        }
       }
-    })
-  })
+    );
+  });
 }
 
+/**
+ * 设置cookie
+ * @param  {string}  url   [需要设置的 Cookie 相关联的 URL]
+ * @param  {string}  name  [cookie 名称]
+ * @param  {string}  value [cookie 值]
+ * @param  {object}  opt  [额外参数]
+ */
 function setCookie(url, name, value, opt = {}) {
-  console.log({
-    url,
-    name,
-    value,
-    ...opt
-  }, 'setCookie')
-  return new Promise(function (res, rej) {
-    chrome.cookies.set(({
-      url,
-      name,
-      value,
-    }),function(cookie) {
-      if (cookie) {
-        res(cookie)
-      } else {
-        rej(false)
+  return new Promise(function (resolve, reject) {
+    chrome.cookies.set(
+      {
+        url,
+        name,
+        value,
+        ...opt,
+      },
+      function (cookie) {
+        if (cookie) {
+          resolve(cookie);
+        } else {
+          reject(false);
+        }
       }
-    })
-  })
+    );
+  });
 }
